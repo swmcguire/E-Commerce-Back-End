981ef815baab481f7bad69bd6ad9534eae7f5f5e
swmcguire <swmcguire1@comcast.net>
2023-01-29 17:50:30 -0600

    updated routes - still need a few more updates, confused about join connections - asking in class Monday

--- models/Tag.js
+++ models/Tag.js
@@ -8,13 +8,13 @@ Tag.init(
   {
     // define columns
     id: {
-      type: DataType.INTEGER, 
+      type: DataTypes.INTEGER, 
       allowNull: false, 
       primaryKey: true, 
       autoIncrement: true
     },
     tag_name: {
-      type: DataType.STRING
+      type: DataTypes.STRING
     }
   },
   {
