199d10d0ee55c0dacb325c33bfe73cd773dd1582
swmcguire <swmcguire1@comcast.net>
2023-02-02 16:54:24 -0600

    fixed syntax error in Category Route

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -7,12 +7,13 @@ const { Category, Product } = require('../../models');
 router.get('/', async (req, res) => {
   try {
     const categoryData = await Category.findAll({
-      include: [{ model: product}],
+      include: [{ model: Product}],
     });
 
     res.status(200).json(categoryData);
   } catch (err) {
     res.status(500).json(err);
+    console.log(categoryData);
   }
 });
 
