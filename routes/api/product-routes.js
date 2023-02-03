bfb009008cf17a18bd3443457bc729159335d56c
swmcguire <swmcguire1@comcast.net>
2023-02-02 18:28:01 -0600

    Updated Post route for product

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -32,7 +32,7 @@ router.get('/:id', async (req, res) => {
       res.status(404).json({ message: `No product with this id!`});
       return;
     }
-    
+
     res.status(200).json(productData);
   } catch (err) {
     res.status(500).json(err);
@@ -41,16 +41,29 @@ router.get('/:id', async (req, res) => {
 
 
 // -------------------- create new product
-router.post('/', (req, res) => {
-  /* req.body should look like this...
+
+router.post('/', async (req, res) => {
+  try{
+    const productData  = await Product.create(req.body);
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+
+
+
+//router.post('/', (req, res) => {
+   /*req.body should look like this...
     {
       product_name: "Basketball",
       price: 200.00,
       stock: 3,
       tagIds: [1, 2, 3, 4]
-    }
-  */
-  Product.create(req.body)
+    }*/
+  
+  /*Product.create(req.body)
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
       if (req.body.tagIds.length) {
@@ -71,11 +84,32 @@ router.post('/', (req, res) => {
       console.log(err);
       res.status(400).json(err);
     });
-});
+});*/
 
 
 // ------------------------------- update product data
-router.put('/:id', (req, res) => {
+
+router.put(':id', async (req, res) =>{
+try {
+  const productData = await Product.update(req.body, {
+    where: {
+      id: req.params.id
+    },
+  });
+  
+  if (!productData) {
+    res.status(404).json({ message: `No product with this id!`});
+    return;
+  }
+
+  res.status(200).json(productData);
+} catch (err) {
+  res.status(400).json(err);
+  }
+});
+
+
+/*router.put('/:id', (req, res) => {
   Product.update(req.body, {
     where: {
       id: req.params.id,
@@ -113,7 +147,7 @@ router.put('/:id', (req, res) => {
       // console.log(err);
       res.status(400).json(err);
     });
-});
+});*/
 
 
 // --------------------------- delete one product by its `id` value
